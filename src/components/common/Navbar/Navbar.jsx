56afa4cc01c650f16cb5cdcf0fca6a91e39ef768
support@prolianceltd.com <support@prolianceltd.com>
2025-07-23 22:19:13 +0100

    Refactor Navbar: Simplify layout, improve responsiveness, and clean up state logic

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -1,133 +1,128 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton, Box, Menu, MenuItem, useMediaQuery, useTheme } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Box,
+  Menu,
+  MenuItem,
+  useMediaQuery,
+  useTheme
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.png';
-// import './Navbar.css';
+
+const navLinks = [
+  { label: 'Courses', path: '/courses' },
+  { label: 'Book a Class', path: '/book-class' },
+  { label: 'About Us', path: '/about' }
+];
 
 const Navbar = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const [profileMenuAnchor, setProfileMenuAnchor] = React.useState(null);
+  const [mobileMenuAnchor, setMobileMenuAnchor] = React.useState(null);
 
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const openProfileMenu = Boolean(profileMenuAnchor);
+  const openMobileMenu = Boolean(mobileMenuAnchor);
 
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+  const handleOpenProfileMenu = (e) => setProfileMenuAnchor(e.currentTarget);
+  const handleCloseProfileMenu = () => setProfileMenuAnchor(null);
 
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
+  const handleOpenMobileMenu = (e) => setMobileMenuAnchor(e.currentTarget);
+  const handleCloseMobileMenu = () => setMobileMenuAnchor(null);
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
+  const renderProfileMenu = (
     <Menu
-      anchorEl={anchorEl}
+      anchorEl={profileMenuAnchor}
+      open={openProfileMenu}
+      onClose={handleCloseProfileMenu}
       anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
       transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
     >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-      <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+      <MenuItem onClick={handleCloseProfileMenu}>Profile</MenuItem>
+      <MenuItem onClick={handleCloseProfileMenu}>My Account</MenuItem>
+      <MenuItem onClick={handleCloseProfileMenu}>Logout</MenuItem>
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
   const renderMobileMenu = (
     <Menu
-      anchorEl={mobileMoreAnchorEl}
+      anchorEl={mobileMenuAnchor}
+      open={openMobileMenu}
+      onClose={handleCloseMobileMenu}
       anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={mobileMenuId}
-      keepMounted
       transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
     >
-      <MenuItem component={Link} to="/courses">Courses</MenuItem>
-      <MenuItem component={Link} to="/book-class">Book a Class</MenuItem>
-      <MenuItem component={Link} to="/about">About Us</MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
+      {navLinks.map(({ label, path }) => (
+        <MenuItem key={label} component={Link} to={path} onClick={handleCloseMobileMenu}>
+          {label}
+        </MenuItem>
+      ))}
+      <MenuItem onClick={(e) => {
+        handleCloseMobileMenu();
+        handleOpenProfileMenu(e);
+      }}>
+        <IconButton size="large" color="inherit">
           <AccountCircle />
         </IconButton>
-        <p>Profile</p>
+        <Typography variant="body1" sx={{ ml: 1 }}>Profile</Typography>
       </MenuItem>
     </Menu>
   );
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="primary" elevation={0}>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link to="/" className="logo-link">
-              <img 
-                src={logo}
-                alt="Learning Management System" 
-                height="50"
-              />
-            </Link>
-          </Typography>
-          
-          {isMobile ? (
-            <>
+      <AppBar position="static" elevation={0} color="primary">
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          {/* Logo */}
+          <Link to="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
+            <img src={logo} alt="Logo" style={{ height: 50 }} />
+          </Link>
+
+          {/* Desktop Menu */}
+          {!isMobile ? (
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+              {navLinks.map(({ label, path }) => (
+                <Button
+                  key={label}
+                  color="inherit"
+                  component={Link}
+                  to={path}
+                >
+                  {label}
+                </Button>
+              ))}
               <IconButton
                 size="large"
                 edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleMobileMenuOpen}
                 color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-            </>
-          ) : (
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Button color="inherit" component={Link} to="/courses">Courses</Button>
-              <Button color="inherit" component={Link} to="/book-class">Book a Class</Button>
-              <Button color="inherit" component={Link} to="/about">About Us</Button>
-              <IconButton
-                size="large"
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit"
+                onClick={handleOpenProfileMenu}
               >
                 <AccountCircle />
               </IconButton>
             </Box>
+          ) : (
+            // Mobile Hamburger
+            <IconButton
+              size="large"
+              edge="end"
+              color="inherit"
+              onClick={handleOpenMobileMenu}
+            >
+              <MenuIcon />
+            </IconButton>
           )}
         </Toolbar>
       </AppBar>
+
       {renderMobileMenu}
-      {renderMenu}
+      {renderProfileMenu}
     </Box>
   );
 };
